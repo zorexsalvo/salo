8b0fb57492ab597c4a7ebdd255af7bf1d2a61881
Zorex Salvo <zorexsalvo@gmail.com>
2018-02-09 13:59:00 +0800

    Pretty print the request body

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from flask import Flask, request
-app = Flask(__name__)
+import json
 
+
+app = Flask(__name__)
 
 @app.route('/api/', methods=['GET', 'POST'])
 def reverse():
     if request.method == 'GET':
         return '8-)'
     else:
+        parsed = json.loads(request.data)
+        print(json.dumps(parsed, indent=4))
         return request.data
 
 
